94bcdc1a9f99a9c3176396ebdc117c59375e8dab
maliavusan <maliavusan@gmail.com>
2023-12-09 20:38:19 +0200

    Refectoring

--- src/main/java/com/management/books/util/HelperUtil.java
+++ src/main/java/com/management/books/util/HelperUtil.java
@@ -14,23 +14,4 @@ public class HelperUtil {
 	public static ResponseEntity<String> returnServerErrorMessage(String errorMessage, String detailedMessage){
 		return new ResponseEntity<>(errorMessage +": " + detailedMessage, HttpStatus.INTERNAL_SERVER_ERROR);
 	}
-
-	//See if you can make use of this
-	// /**
-	//  * Method to build server error message
-	//  * @param errorMessage
-	//  * @return
-	//  */
-	// public static ResponseEntity<String> returnServerErrorMessage(String errorMessage){
-	// 	return new ResponseEntity<>(errorMessage, HttpStatus.INTERNAL_SERVER_ERROR);
-	// }
-
-	// /**
-	//  * Method to build bad request error message
-	//  * @param errorMessage
-	//  * @return
-	//  */
-	// public static ResponseEntity<String> returnForbiddenRequestErrorMessage(String errorMessage){
-	// 	return new ResponseEntity<>(errorMessage, HttpStatus.FORBIDDEN);
-	// }
 }
